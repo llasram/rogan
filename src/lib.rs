48e2a29b938662924121477bb20c5bcc3e41371a
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2015-12-15 20:11:23 -0500

    Refactoring; placing orders WIP

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,8 @@ use std::fmt;
 use std::result;
 
 use chrono::{DateTime, UTC};
-use hyper::client::{Client, RequestBuilder};
+use hyper::client::{Body, Client, RequestBuilder};
+use hyper::method::Method;
 use hyper::status::StatusCode;
 
 header! {
@@ -92,6 +93,10 @@ pub type Result<T> = result::Result<T, Error>;
 
 static DEFAULT_API_URL: &'static str = "https://api.stockfighter.io/ob/api";
 
+fn error_str(error: Option<String>) -> String {
+    error.unwrap_or_else(|| "<unknown>".to_owned())
+}
+
 pub struct Api {
     url: String,
     token: String,
@@ -123,20 +128,37 @@ impl Api {
         format!("{}/{}", self.url, url)
     }
 
-    fn get(&self, url: &str) -> RequestBuilder {
+    fn request(&self, method: Method, url: &str) -> RequestBuilder {
         let auth = XStarfighterAuthorization(self.token.clone());
-        self.client.get(&self.url(url)).header(auth)
+        self.client.request(method, &self.url(url)).header(auth)
     }
 
     pub fn heartbeat(&self) -> Result<()> {
-        let res = try!(self.get("heartbeat").send());
+        let res = try!(self.request(Method::Get, "heartbeat").send());
         let status = res.status;
         let hb: HeartbeatResponse = try!(serde_json::de::from_reader(res));
-        let error = hb.error.unwrap_or_else(|| "<none>".to_owned());
         match (status, hb.ok) {
             (StatusCode::Ok, true) => Ok(()),
-            _ => Err(Error::Unknown(error)),
+            _ => Err(Error::Unknown(error_str(hb.error))),
         }
+    }
+
+    pub fn account(&self, name: &str) -> Result<Account> {
+        let name = name.to_owned();
+        let account = Account { api: self, name: name };
+        Ok(account)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Account<'a> {
+    api: &'a Api,
+    pub name: String,
+}
+
+impl<'a> Account<'a> {
+    pub fn request(&self, method: Method, url: &str) -> RequestBuilder {
+        self.api.request(method, url)
     }
 
     pub fn venue(&self, name: &str) -> Result<Venue> {
@@ -144,15 +166,10 @@ impl Api {
     }
 }
 
+#[derive(Debug)]
 pub struct Venue<'a> {
-    api: &'a Api,
+    account: &'a Account<'a>,
     pub name: String,
-}
-
-impl<'a> fmt::Debug for Venue<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Venue {{ name: {:?} }}", self.name)
-    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -176,8 +193,8 @@ struct VenueStocksResponse {
 }
 
 impl<'a> Venue<'a> {
-    fn new(api: &'a Api, name: &str) -> Result<Self> {
-        let venue = Venue { api: api, name: name.to_owned() };
+    fn new(account: &'a Account, name: &str) -> Result<Self> {
+        let venue = Venue { account: account, name: name.to_owned() };
         try!(venue.heartbeat());
         Ok(venue)
     }
@@ -186,28 +203,28 @@ impl<'a> Venue<'a> {
         format!("venues/{}/{}", self.name, url)
     }
 
-    fn get(&self, url: &str) -> RequestBuilder {
-        self.api.get(&self.url(url))
+    fn request(&self, method:Method, url: &str) -> RequestBuilder {
+        self.account.request(method, &self.url(url))
     }
 
     pub fn heartbeat(&self) -> Result<()> {
-        let res = try!(self.get("heartbeat").send());
+        let res = try!(self.request(Method::Get, "heartbeat").send());
         let status = res.status;
         let hb: VenueHeartbeatResponse = try!(serde_json::de::from_reader(res));
-        let error = hb.error.unwrap_or_else(|| "<none>".to_owned());
+        let error = hb.error;
         match (status, hb.ok) {
             (StatusCode::Ok, true) => Ok(()),
-            (StatusCode::InternalServerError, false) => Err(Error::Timeout(error)),
-            (StatusCode::NotFound, false) => Err(Error::NotFound(error)),
-            _ => Err(Error::Unknown(error)),
+            (StatusCode::InternalServerError, false) => Err(Error::Timeout(error_str(error))),
+            (StatusCode::NotFound, false) => Err(Error::NotFound(error_str(error))),
+            _ => Err(Error::Unknown(error_str(error))),
         }
     }
 
     pub fn stocks(&self) -> Result<Vec<Stock>> {
-        let res = try!(self.get("stocks").send());
+        let res = try!(self.request(Method::Get, "stocks").send());
         let status = res.status;
         let vs: VenueStocksResponse = try!(serde_json::de::from_reader(res));
-        let error = vs.error.unwrap_or_else(|| "<none>".to_owned());
+        let error = vs.error;
         match (status, vs.ok, vs.symbols) {
             (StatusCode::Ok, true, Some(symbols)) => {
                 let stocks = symbols.into_iter().map(|stock| {
@@ -215,8 +232,8 @@ impl<'a> Venue<'a> {
                 }).collect::<Vec<_>>();
                 Ok(stocks)
             },
-            (StatusCode::NotFound, false, None) => Err(Error::NotFound(error)),
-            _ => Err(Error::Unknown(error)),
+            (StatusCode::NotFound, false, None) => Err(Error::NotFound(error_str(error))),
+            _ => Err(Error::Unknown(error_str(error))),
         }
     }
 
@@ -228,14 +245,14 @@ impl<'a> Venue<'a> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Stock<'a> {
     pub venue: &'a Venue<'a>,
     pub symbol: String,
     pub name: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Copy, Clone)]
 pub struct StockOrder {
     pub price: u64,
     pub qty: u64,
@@ -254,39 +271,129 @@ struct StockOrdersResponse {
     asks: Option<Vec<StockOrder>>,
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum Direction {
+    Buy,
+    Sell,
+}
+
+impl Direction {
+    pub fn as_str(&self) -> &'static str {
+        match *self {
+            Direction::Buy => "buy",
+            Direction::Sell => "sell",
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum OrderType {
+    Limit,
+    Market,
+    FillOrKill,
+    ImmediateOrCancel,
+}
+
+impl OrderType {
+    pub fn as_str(&self) -> &'static str {
+        match *self {
+            OrderType::Limit => "limit",
+            OrderType::Market => "market",
+            OrderType::FillOrKill => "fill-or-kill",
+            OrderType::ImmediateOrCancel => "immediate-or-cancel",
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct OrderRequest<'a> {
+    pub account: &'a str,
+    pub venue: &'a str,
+    pub stock: &'a str,
+    pub price: u64,
+    pub qty: u64,
+    pub direction: &'static str,
+    #[serde(rename="orderType")]
+    pub order_type: &'static str,
+}
+
 impl<'a> Stock<'a> {
     fn url(&self) -> String {
         format!("stocks/{}", self.symbol)
     }
 
-    fn get(&self) -> RequestBuilder {
-        self.venue.get(&self.url())
+    fn request(&self, method: Method) -> RequestBuilder {
+        self.venue.request(method, &self.url())
     }
 
     pub fn orders(&self) -> Result<StockOrders> {
-        let res = try!(self.get().send());
+        let res = try!(self.request(Method::Get).send());
         let status = res.status;
         let so: StockOrdersResponse = try!(serde_json::de::from_reader(res));
-        let error = so.error.unwrap_or_else(|| "<none>".to_owned());
+        let error = so.error;
         match (status, so.ok, so.ts, so.bids, so.asks) {
             (StatusCode::Ok, true, Some(ts), Some(bids), Some(asks)) => {
                 let ts = try!(ts.parse::<DateTime<UTC>>());
                 let orders = StockOrders { stock: self, ts: ts, bids: bids, asks: asks };
                 Ok(orders)
             },
-            (StatusCode::NotFound, false, None, None, None) => Err(Error::NotFound(error)),
-            _ => Err(Error::Unknown(error)),
+            (StatusCode::NotFound, false, _, _, _) => Err(Error::NotFound(error_str(error))),
+            _ => Err(Error::Unknown(error_str(error))),
         }
+    }
+
+    pub fn order(&self, price: u64, qty: u64, direction: Direction, order_type: OrderType)
+                 -> Result<OrderStatus> {
+        let direction = direction.as_str();
+        let order_type = order_type.as_str();
+        let req = OrderRequest {
+            account: &self.venue.account.name, venue: &self.venue.name, stock: &self.name,
+            price: price, qty: qty, direction: direction, order_type: order_type,
+        };
+        let req = try!(serde_json::to_string(&req));
+        let res = try!(self.request(Method::Post).body(&*req).send());
+        let status = res.status;
+        Ok(OrderStatus)
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct StockOrders<'a> {
     pub stock: &'a Stock<'a>,
     pub ts: DateTime<UTC>,
     pub bids: Vec<StockOrder>,
     pub asks: Vec<StockOrder>,
 }
+
+#[derive(Debug, Deserialize)]
+struct FillResponse {
+    price: u64,
+    qty: u64,
+    ts: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct OrderStatusResponse {
+    ok: bool,
+    symbol: String,
+    venue: String,
+    direction: String,
+    #[serde(rename="originalQty")]
+    original_qty: u64,
+    qty: u64,
+    price: u64,
+    #[serde(rename="type")]
+    order_type: String,
+    id: u64,
+    account: String,
+    ts: String,
+    fills: Vec<FillResponse>,
+    #[serde(rename="totalFilled")]
+    total_filled: u64,
+    open: bool,
+}
+
+pub struct OrderStatus;
 
 #[cfg(test)]
 mod tests {
@@ -302,15 +409,17 @@ mod tests {
 
     #[test]
     fn test_venue_heartbeat() {
-        let client = Api::new(TOKEN);
-        let venue = client.venue("TESTEX").unwrap();
+        let api = Api::new(TOKEN);
+        let account = api.account("EXB123456").unwrap();
+        let venue = account.venue("TESTEX").unwrap();
         assert!(venue.heartbeat().is_ok());
     }
 
     #[test]
     fn test_venue_stocks() {
-        let client = Api::new(TOKEN);
-        let venue = client.venue("TESTEX").unwrap();
+        let api = Api::new(TOKEN);
+        let account = api.account("EXB123456").unwrap();
+        let venue = account.venue("TESTEX").unwrap();
         let stocks = venue.stocks().unwrap();
         assert_eq!(1, stocks.len());
         assert_eq!("FOOBAR", stocks[0].symbol);
@@ -318,8 +427,9 @@ mod tests {
 
     #[test]
     fn test_stock_orders() {
-        let client = Api::new(TOKEN);
-        let venue = client.venue("TESTEX").unwrap();
+        let api = Api::new(TOKEN);
+        let account = api.account("EXB123456").unwrap();
+        let venue = account.venue("TESTEX").unwrap();
         let stock = venue.stock("FOOBAR").unwrap();
         let orders = stock.orders().unwrap();
         assert!(0 < orders.bids.len());
