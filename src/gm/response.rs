d94bff783f315e0f3781ad8efdafc80dd0bc1a5c
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2015-12-29 18:14:50 -0500

    Add an enum for game state

--- src/gm/response.rs
+++ src/gm/response.rs
@@ -1,4 +1,7 @@
 use std::collections::BTreeMap;
+use std::str::FromStr;
+
+use common::{Error, Result};
 
 #[derive(Debug, Deserialize)]
 pub struct Instance {
@@ -15,7 +18,7 @@ pub struct Instance {
     pub balances: Option<BTreeMap<String, u64>>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct StatusDetails {
     #[serde(rename="endOfTheWorldDay")]
     pub end_of_the_world_day: u64,
@@ -25,13 +28,35 @@ pub struct StatusDetails {
     pub execution_time: Option<f64>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct StatusFlash {
     pub info: Option<String>,
+    pub warning: Option<String>,
     pub error: Option<String>,
+    pub success: Option<String>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum StatusState {
+    Open,
+    Won,
+    Lost,
+}
+
+impl FromStr for StatusState {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<StatusState> {
+        match s {
+            "open" => Ok(StatusState::Open),
+            "won" => Ok(StatusState::Won),
+            "lost" => Ok(StatusState::Lost),
+            _ => Err(Error::Unknown(format!("{}: invalid `state`", s))),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Clone)]
 pub struct Status {
     pub ok: bool,
     pub error: Option<String>,
@@ -40,6 +65,16 @@ pub struct Status {
     pub state: String,
     pub flash: Option<StatusFlash>,
     pub details: Option<StatusDetails>,
+}
+
+impl Status {
+    pub fn trading_day(&self) -> u64 {
+        self.details.as_ref().map_or(0, |details| details.trading_day)
+    }
+
+    pub fn state(&self) -> Result<StatusState> {
+        self.state.parse::<StatusState>()
+    }
 }
 
 #[derive(Debug, Deserialize)]
